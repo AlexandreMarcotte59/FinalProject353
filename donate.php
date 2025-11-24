4db23caf8ddec40766a6e75cdbedde1c7f22ffa5
OlgertaGjyriqi <olgertagjyriqi@gmail.com>
2025-11-24 13:59:15 -0500

    Update donate.php

--- donate.php
+++ donate.php
@@ -1,8 +1,10 @@
 <?php
-$servername="localhost"; 
-$username="your_encs_username"; 
-$password="your_encs_mysql_password"; 
-$dbname="your_encs_username_db";
+
+$servername="mvc353.encs.concordia.ca"; 
+$username="mvc353_2"; 
+$password="firstsound58"; 
+$dbname="mvc353_2";
+
 $conn=new mysqli($servername,$username,$password,$dbname);
 if($conn->connect_error) die("Connection failed: ".$conn->connect_error);
 
@@ -50,3 +52,4 @@ $conn->close();
 ?>
 </table>
 </body></html>
+
