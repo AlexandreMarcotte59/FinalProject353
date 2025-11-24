0cdb93d10f6406293e732d011b6bf3093f86bc37
OlgertaGjyriqi <olgertagjyriqi@gmail.com>
2025-11-24 14:01:48 -0500

    Update statistics.php

--- statistics.php
+++ statistics.php
@@ -1,7 +1,10 @@
 <?php
-$servername="localhost"; $username="your_encs_username"; 
-$password="your_encs_mysql_password"; $dbname="your_encs_username_db";
-$conn=new mysqli($servername,$username,$password,$dbname);
+
+$servername="mvc353.encs.concordia.ca"; 
+$username="mvc353_2"; 
+$password="firstsound58"; 
+$dbname="mvc353_2";
+
 if($conn->connect_error) die("Connection failed: ".$conn->connect_error);
 ?>
 <!DOCTYPE html><html><body>
@@ -22,3 +25,4 @@ $conn->close();
 ?>
 </table>
 </body></html>
+
