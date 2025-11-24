fb5e8663520bf59a3648e3e9332307426547479a
OlgertaGjyriqi <olgertagjyriqi@gmail.com>
2025-11-24 13:54:13 -0500

    Update add_comment.php

--- add_comment.php
+++ add_comment.php
@@ -1,9 +1,9 @@
 <?php
 // CONNECT
-$servername = "localhost";
-$username = "your_encs_username";
-$password = "your_encs_mysql_password";
-$dbname   = "your_encs_username_db";
+$servername = "mvc353.encs.concordia.ca";
+$username = "mvc353_2";
+$password = "firstsound58";
+$dbname   = "mvc353";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) die("Connection failed: " . $conn->connect_error);
@@ -51,3 +51,4 @@ $conn->close();
 ?>
 </table>
 </body></html>
+
