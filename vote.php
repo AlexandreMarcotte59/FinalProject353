39bc27f5ca129c6eb1e53497cacf3e47be93558a
OlgertaGjyriqi <olgertagjyriqi@gmail.com>
2025-11-24 14:02:35 -0500

    Update vote.php

--- vote.php
+++ vote.php
@@ -1,7 +1,10 @@
 <?php
-$servername="localhost"; $username="your_encs_username"; 
-$password="your_encs_mysql_password"; $dbname="your_encs_username_db";
-$conn=new mysqli($servername,$username,$password,$dbname);
+
+$servername="mvc353.encs.concordia.ca"; 
+$username="mvc353_2"; 
+$password="firstsound58"; 
+$dbname="mvc353_2";
+
 if($conn->connect_error) die("Connection failed: ".$conn->connect_error);
 
 if($_SERVER["REQUEST_METHOD"]=="POST"){
@@ -44,3 +47,4 @@ $conn->close();
 ?>
 </table>
 </body></html>
+
